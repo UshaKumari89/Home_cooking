efb8528b0531298fda2bf64edea5a11e85e1b806
Usha Kumari <uavaswani@gmail.com>
2022-07-31 20:29:35 +0200

    login form

---
+++ index.js
@@ -0,0 +1,36 @@
+let id = (id) => document.getElementById(id);
+let classes = (classes) => document.getElementsByClassName(classes);
+
+let userFirstName = id("userFirstName"),
+    userLastName = id("userLastName"),
+    email = id("email"),
+    password = id("password"),
+    form = id("form"),
+    errorMsg = classes("error"),
+    successIcon = classes("success-icon"),
+    failureIcon =  classes("failure-icon");
+   
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+      check(userFirstName, 0 , "First name cannot be blank");
+      check(userFirstName, 1 , "Last name cannot be blank");
+      check(email, 2, "Email cannot be blank");
+      check(password, 3 , "Password cannot be blank");
+       
+
+    })
+    function check(id , serial , message){
+
+        if(id.value.trim() === ''){
+            errorMsg[serial].innerHTML = message;
+            failureIcon[serial].style.opacity = "1";
+            successIcon[serial].style.opacity = "0";
+    
+            }else{
+                errorMsg[serial].innerHTML = " ";
+                failureIcon[serial].style.opacity = "0";
+                successIcon[serial].style.opacity = "1";
+            }
+    }
